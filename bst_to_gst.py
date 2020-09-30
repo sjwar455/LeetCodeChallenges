9dd6a6158a45148f9b61a17e57ecf0da3ad40ccb
Samantha Wareing <samantha.wareing@gmail.com>
2020-09-30 15:37:55 -0700

    initial commit for bst to gst

---
+++ bst_to_gst.py
@@ -0,0 +1,22 @@
+############################################################################################################################
+# File: bst_to_gst.py
+# Author: Sam Wareing
+# Description: my solution to LeetCode challenge 1038 
+# Challenge:  
+#
+# Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original 
+# BST is changed to the original key plus sum of all keys greater than the original key in BST.
+#
+# As a reminder, a binary search tree is a tree that satisfies these constraints:
+#
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+#
+# Constraints:
+#
+# The number of nodes in the tree is in the range [1, 100].
+# 0 <= Node.val <= 100
+# All the values in the tree are unique.
+# root is guaranteed to be a valid binary search tree.
+############################################################################################################################
